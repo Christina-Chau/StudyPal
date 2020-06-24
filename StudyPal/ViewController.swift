9731c26b217350598b83e1a22cea3a877b13583b
Christina-Chau <christinachau5@gmail.com>
2020-06-24 17:22:52 -0400

    Initial Commit

---
+++ StudyPal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyPal
+//
+//  Created by Christina Chau on 6/24/20.
+//  Copyright © 2020 Christina Chau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
